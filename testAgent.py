dd006d6efc7b39b51e95e299d3fb4a46d30d4296
catherinelee274 <catherinelee274@yahoo.com>
2020-07-09 19:58:14 -0700

    change

--- testAgent.py
+++ testAgent.py
@@ -6,10 +6,14 @@ env = gym.make('ArcheryEnv') # call our env
 env.reset()
 
 for _ in range(1000):
+    observation = env.reset()
     env.render()
     # print observation
     action = env.action_space.sample()
     observation, reward, done, info = env.step(action)
+    if done:
+        print("Episode finished after {} timesteps".format(t+1))
+        break
     
 env.close()
 
